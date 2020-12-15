32406755ecfe41476297588dc3ec712da2ca926b
Tako <pierrelouis.hernio@gmail.com>
2020-12-15 12:42:51 +0100

    Added QR code / bar code reader + dependency

---
+++ app/src/main/java/com/enib/toolty/ui/scan/Capture.java
@@ -0,0 +1,6 @@
+package com.enib.toolty.ui.scan;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+}
--- app/src/main/java/com/enib/toolty/ui/scan/ScanFragment.java
+++ app/src/main/java/com/enib/toolty/ui/scan/ScanFragment.java
@@ -1,12 +1,17 @@
 package com.enib.toolty.ui.scan;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,8 +23,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.enib.toolty.MainActivity;
 import com.enib.toolty.R;
 import com.enib.toolty.ui.Pedometer.PedometerViewModel;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+import static com.google.zxing.integration.android.IntentIntegrator.forSupportFragment;
 
 public class ScanFragment extends Fragment {
 
@@ -39,8 +50,14 @@ public class ScanFragment extends Fragment {
         camBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
+                IntentIntegrator intentIntegrator = IntentIntegrator.forSupportFragment(ScanFragment.this);
+
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.setBarcodeImageEnabled(true);
+                intentIntegrator.setPrompt("Scannez un QR code ou un code bar !");
+                intentIntegrator.initiateScan();
             }
         });
 
@@ -51,9 +68,25 @@ public class ScanFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        if(bitmap == null) {return;}
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(
+                requestCode, resultCode, data
+        );
 
-        imageView.setImageBitmap(bitmap);
+        if(intentResult.getContents() != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    getActivity()
+            );
+
+            builder.setTitle("Résultat du scan");
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("Fermer", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            builder.show();
+        }
     }
 }
